3ddebae5d41e076a16afa12df69bcbd824f69e32
Anna Tkach <115637580+Tomato6soup@users.noreply.github.com>
2026-03-03 17:46:54 +0100

    tworzenie programu w VS

---
+++ ProjektZP_Tkach/ProjektZP_Tkach/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProjektZP_Tkach
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
