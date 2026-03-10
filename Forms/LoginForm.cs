1289b2d3d458170a75a768e50842997f245dc094
polina-npc <taeterrati@gmail.com>
2026-03-10 14:38:32 +0100

    Login + Main (forms)

---
+++ Forms/LoginForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+public partial class LoginForm : Form
+{
+    private readonly LoginViewModel viewModel = new();
+    private TextBox txtLogin;
+    private TextBox txtHaslo;
+    private Button btnLogin;
+
+    public LoginForm()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        txtLogin = new TextBox { Left = 50, Top = 20, Width = 200, PlaceholderText = "Login" };
+        txtHaslo = new TextBox { Left = 50, Top = 60, Width = 200, UseSystemPasswordChar = true, PlaceholderText = "Hasło" };
+        btnLogin = new Button { Left = 100, Top = 100, Width = 100, Text = "Zaloguj" };
+        btnLogin.Click += btnLogin_Click;
+
+        Controls.Add(txtLogin);
+        Controls.Add(txtHaslo);
+        Controls.Add(btnLogin);
+
+        Text = "Logowanie";
+        Width = 320;
+        Height = 200;
+        StartPosition = FormStartPosition.CenterScreen;
+    }
+
+    private void btnLogin_Click(object sender, EventArgs e)
+    {
+        viewModel.Login = txtLogin.Text;
+        viewModel.Haslo = txtHaslo.Text;
+
+        if (viewModel.Zaloguj())
+        {
+            Hide();
+            var mainForm = new MainForm();
+            mainForm.Show();
+        }
+        else
+        {
+            MessageBox.Show("Błędny login lub hasło.");
+        }
+    }
+}
---
+++ Forms/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+public partial class MainForm : Form
+{
+    private readonly PublikacjeViewModel viewModel = new();
+    private DataGridView dgvPublikacje;
+
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        dgvPublikacje = new DataGridView { Dock = DockStyle.Fill, AutoGenerateColumns = true };
+        Controls.Add(dgvPublikacje);
+        Load += MainForm_Load;
+
+        Text = "Publikacje";
+        Width = 800;
+        Height = 600;
+        StartPosition = FormStartPosition.CenterScreen;
+    }
+
+    private void MainForm_Load(object? sender, EventArgs e)
+    {
+        viewModel.ZaladujPublikacje();
+        dgvPublikacje.DataSource = viewModel.Publikacje;
+    }
+}
