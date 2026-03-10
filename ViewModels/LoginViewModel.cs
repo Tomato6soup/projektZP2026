aaf8fefb51425d648f557882938b8946219cafbd
polina-npc <taeterrati@gmail.com>
2026-03-10 16:48:58 +0100

    ViewModel (login + Publ)

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+
+public class LoginViewModel : BaseViewModel
+{
+    private string _login = string.Empty;
+    public string Login
+    {
+        get => _login;
+        set { _login = value; OnPropertyChanged(); }
+    }
+
+    private string _haslo = string.Empty;
+    public string Haslo
+    {
+        get => _haslo;
+        set { _haslo = value; OnPropertyChanged(); }
+    }
+
+    public bool Zaloguj()
+    {
+        string connStr = "Data Source=.\\SQLEXPRESS;Initial Catalog=TwojaBazaDanych;Integrated Security=True";
+        using SqlConnection conn = new(connStr);
+        conn.Open();
+
+        string query = "SELECT COUNT(*) FROM Uzytkownik WHERE Login = @login AND Haslo = @haslo";
+        using SqlCommand cmd = new(query, conn);
+        cmd.Parameters.AddWithValue("@login", Login);
+        cmd.Parameters.AddWithValue("@haslo", Haslo);
+
+        int count = (int)cmd.ExecuteScalar();
+        return count > 0;
+    }
+}
---
+++ ViewModels/PublikacjeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using BazaPublikacji_app.Models;
+using Microsoft.Data.SqlClient;
+
+public class PublikacjeViewModel : BaseViewModel
+{
+    public BindingList<Publikacja> Publikacje { get; set; } = new();
+
+    public void ZaladujPublikacje()
+    {
+        string connStr = "Data Source=.\\SQLEXPRESS;Initial Catalog=TwojaBazaDanych;Integrated Security=True";
+        using SqlConnection conn = new(connStr);
+        conn.Open();
+
+        string query = "SELECT ID, Tytul, Rok_Wydania FROM Publikacja";
+        using SqlCommand cmd = new(query, conn);
+        using SqlDataReader reader = cmd.ExecuteReader();
+
+        Publikacje.Clear();
+        while (reader.Read())
+        {
+            Publikacje.Add(new Publikacja
+            {
+                ID = reader.GetInt32(0),
+                Tytul = reader.GetString(1),
+                RokWydania = reader.GetInt32(2)
+            });
+        }
+    }
+}
