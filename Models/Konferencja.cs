4131f2396523c63b33294b1bb02e7c5ab9e80a51
Anna Tkach <115637580+Tomato6soup@users.noreply.github.com>
2026-03-16 23:21:23 +0100

    Dodany Model Konferencja

---
+++ Models/Konferencja.cs
@@ -0,0 +1,7 @@
+﻿public class Konferencja
+{
+    public int ID { get; set; }
+    public string Nazwa { get; set; } = string.Empty;
+    public DateTime Data { get; set; }
+    public string Miejsce { get; set; } = string.Empty;
+}
