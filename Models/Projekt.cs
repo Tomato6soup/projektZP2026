43ff31cce254874e94a8b1e08c2d0c2e992cdf1c
polina-npc <taeterrati@gmail.com>
2026-03-16 23:18:15 +0100

    Model Projekt

---
+++ Models/Projekt.cs
@@ -0,0 +1,9 @@
+﻿public class Projekt
+{
+    public int ID { get; set; }
+    public string Tytul { get; set; } = string.Empty;
+    public string Opis { get; set; } = string.Empty;
+    public DateTime DataRozpoczecia { get; set; }
+    public DateTime DataZakonczenia { get; set; }
+
+}
