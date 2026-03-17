50f094ffa95201d15da1700bbdcb4fc56dc4903a
irczyk <s61036@student.ubb.edu.pl>
2026-03-17 17:08:51 +0100

    model uzytkownik

---
+++ Models/Uzytkownik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazaPublikacji_app.Models
+{
+    public class Uzytkownik
+    {
+        public int ID { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string Haslo { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Rola { get; set; } = string.Empty; //  "Administrator" lub "Student"
+    }
+
+}
